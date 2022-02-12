84d496c217d27a0d0b7d44e40db76d57bdc18fa2
jrekier <jrekier@gmail.com>
2022-02-12 14:23:05 +0100

    misc

---
+++ src/_data/communications.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    'date': 'July 2012',
+    'event': '13th Marcel Grossmann Meeting, Stockholm, Sweden',
+    'title': 'Cosmic acceleration with cosmological soft phonons'
+  },
+  {
+    'date': 'September 2014',
+    'event': 'Spanish Relativity Meeting ERE, Valencia, Spain',
+    'title': 'Spherically Symmetric solutions on a cosmological dynamical background with BSSN equations',
+    'proceding': 'http://dx.doi.org/10.1088/1742-6596/600/1/012062'
+  },
+  {
+    'date': 'July 2016',
+    'event': 'GAGER Meeting, Wuhan, China',
+    'title': 'Coupling of the Earth inertial modes to its rotational modes'
+  }
+]
