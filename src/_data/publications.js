26c0814c1d68ed0f42a55f509c315de55cd0b613
jrekier <jrekier@gmail.com>
2024-02-19 12:24:47 -0800

    update list of publications

--- src/_data/publications.js
+++ src/_data/publications.js
@@ -1,68 +1,94 @@
 module.exports = [
   {
-    'reference': "Clesse, S., & Rekier, J. (2014).Updated constraints on large field hybrid inflation. Physical Review D, 90(8), 1–13.",
-    'url': 'https://doi.org/10.1103/PhysRevD.90.083527', 
-    'arxiv': '[arXiv:1407.1984]',
-    'arxivurl': 'https://arxiv.org/abs/1407.1984'
+    "reference": "Clesse, S., & Rekier, J. (2014) Updated constraints on large field hybrid inflation. Phys. Rev. D 90, 1–13.",
+    "url": "https://doi.org/10.1103/PhysRevD.90.083527",
+    "arxiv": "[arXiv:1407.1984]",
+    "arxivurl": "https://arxiv.org/abs/1407.1984"
   },
   {
-    'reference': "Rekier, J., Cordero-Carrión, I., & Füzfa, A. (2015). Fully relativistic non-linear cosmological evolution in spherical symmetry using the BSSN formalism. Physical Review D - Particles, Fields, Gravitation and Cosmology, 91(2), 1–11.",
-    'url': 'https://doi.org/10.1103/PhysRevD.91.024025', 
-    'arxiv': '[arXiv:1409.3476]',
-    'arxivurl': 'https://arxiv.org/abs/1409.3476'
+    "reference": "Rekier, J., Cordero-Carrión, I., & Füzfa, A. (2015) Fully relativistic non-linear cosmological evolution in spherical symmetry using the BSSN formalism. Phys. Rev. D 91, 1–11.",
+    "url": "https://doi.org/10.1103/PhysRevD.91.024025",
+    "arxiv": "[arXiv:1409.3476]",
+    "arxivurl": "https://arxiv.org/abs/1409.3476"
   },
   {
-    'reference': "Rekier, J., Füzfa, A., & Cordero-Carrión, I. (2016). Nonlinear cosmological spherical collapse of quintessence. Physical Review D, 93(4).",
-    'url': 'https://doi.org/10.1103/PhysRevD.93.043533', 
-    'arxiv': '[arXiv:1509.08354]',
-    'arxivurl': 'https://arxiv.org/abs/1509.08354'
+    "reference": "Rekier, J., Füzfa, A., & Cordero-Carrión, I. (2016) Nonlinear cosmological spherical collapse of quintessence. Phys. Rev. D 93.",
+    "url": "https://doi.org/10.1103/PhysRevD.93.043533",
+    "arxiv": "[arXiv:1509.08354]",
+    "arxivurl": "https://arxiv.org/abs/1509.08354"
   },
   {
-    'reference': "Dehant, V., Laguerre, R., Rekier, J., Rivoldini, A., Triana, S. A., Trinh, A., … Zhu, P. (2017). Understanding the effects of the core on the nutation of the Earth. Geodesy and Geodynamics.",
-    'url': 'https://doi.org/10.1016/j.geog.2017.04.005', 
+    "reference": "Dehant, V., Laguerre, R., Rekier, J., et al. (2017) Understanding the effects of the core on the nutation of the Earth. Geod. Geodyn.",
+    "url": "https://doi.org/10.1016/j.geog.2017.04.005"
   },
   {
-    'reference': "Rekier, J., Trinh, A., Triana, S. A., & Dehant, V. (2018). Inertial modes in near-spherical geometries. Geophysical Journal International, 216(2), 777–793.",
-    'url': 'https://doi.org/10.1093/gji/ggy465', 
-    'arxiv': '[arXiv:1904.05221]',
-    'arxivurl': 'https://arxiv.org/abs/1904.05221'
+    "reference": "Rekier, J., Trinh, A., Triana, S. A., & Dehant, V. (2018) Inertial modes in near-spherical geometries. Geophys. J. Int. 216, 777–793.",
+    "url": "https://doi.org/10.1093/gji/ggy465",
+    "arxiv": "[arXiv:1904.05221]",
+    "arxivurl": "https://arxiv.org/abs/1904.05221"
   },
   {
-    'reference': "Triana, S. A., Rekier, J., Trinh, A., & Dehant, V. (2019). The coupling between inertial and rotational eigenmodes in planets with liquid cores. Geophysical Journal International, 218(2), 1071–1086.",
-    'url': 'https://doi.org/10.1093/gji/ggz212', 
-    'arxiv': '[arXiv:1904.11837]',
-    'arxivurl': 'https://arxiv.org/abs/1904.11837'
+    "reference": "Triana, S. A., Rekier, J., Trinh, A., & Dehant, V. (2019) The coupling between inertial and rotational eigenmodes in planets with liquid cores. Geophys. J. Int. 218, 1071–1086.",
+    "url": "https://doi.org/10.1093/gji/ggz212",
+    "arxiv": "[arXiv:1904.11837]",
+    "arxivurl": "https://arxiv.org/abs/1904.11837"
   },
   {
-    'reference': "Rekier, J., Trinh, A., Triana, S. A., & Dehant, V. (2019). Internal energy dissipation in Enceladus's subsurface ocean from tides and libration & the role of inertial waves. Journal of Geophysical Research: Planets, 124.",
-    'url': 'https://doi.org/10.1029/2019JE005988', 
-    'arxiv': '[arXiv:1904.02487]',
-    'arxivurl': 'https://arxiv.org/abs/1904.02487'
+    "reference": "Rekier, J., Trinh, A., Triana, S. A., & Dehant, V. (2019) Internal energy dissipation in Enceladus's subsurface ocean from tides and libration & the role of inertial waves. J. Geophys. Res.: Planets 124.",
+    "url": "https://doi.org/10.1029/2019JE005988",
+    "arxiv": "[arXiv:1904.02487]",
+    "arxivurl": "https://arxiv.org/abs/1904.02487"
   },
   {
-    'reference': 'Rekier, J., Trinh, A., Triana, S. A., & Dehant, V. (2020). Inertial modes of a freely rotating ellipsoidal planet and their relation to nutations. The Planetary Science Journal, 1(1), 20',
-    'url': 'https://doi.org/10.3847/PSJ/ab93c8',
-    'arxiv': '[arXiv:2004.08218]',
-    'arxivurl': 'https://arxiv.org/abs/2004.08218'
+    "reference": "Rekier, J., Trinh, A., Triana, S. A., & Dehant, V. (2020) Inertial modes of a freely rotating ellipsoidal planet and their relation to nutations. Planetary Sci. J. 1, 20.",
+    "url": "https://doi.org/10.3847/PSJ/ab93c8",
+    "arxiv": "[arXiv:2004.08218]",
+    "arxivurl": "https://arxiv.org/abs/2004.08218"
   },
   {
-    'reference': 'Staelens, F., Rekier, J. & Füzfa, A. Universality of the spherical collapse with respect to the matter type: the case of a barotropic fluid with linear equation of state. Gen Relativ Gravit 53, 38 (2021).',
-    'url': 'https://doi.org/10.1007/s10714-021-02804-4',
+    "reference": "Staelens, F., Rekier, J., & Füzfa, A. (2021) Universality of the spherical collapse with respect to the matter type: the case of a barotropic fluid with linear equation of state. Gen. Relativ. Gravit. 53, 38.",
+    "url": "https://doi.org/10.1007/s10714-021-02804-4"
   },
   {
-    'reference': "Triana, S. A., Trinh, A., Rekier, J., Zhu, P., & Dehant, V. (2021). The viscous and Ohmic damping of the Earth's free core nutation. Journal of Geophysical Research: Solid Earth, 126, e2020JB021042. ",
-    'url': 'https://doi.org/10.1029/2020JB021042'
+    "reference": "Triana, S. A., Trinh, A., Rekier, J., et al. (2021) The viscous and Ohmic damping of the Earth's free core nutation. J. Geophys. Res.: Solid Earth 126.",
+    "url": "https://doi.org/10.1029/2020JB021042"
   },
   {
-    'reference': 'Zhu, P., Triana, S.A., Rekier, J. et al. Quantification of corrections for the main lunisolar nutation components and analysis of the free core nutation from VLBI-observed nutation residuals. J Geod 95, 57 (2021).',
-    'url': 'https://doi.org/10.1007/s00190-021-01513-9'
+    "reference": "Zhu, P., Triana, S.A., Rekier, J., et al. (2021) Quantification of corrections for the main lunisolar nutation components and analysis of the free core nutation from VLBI-observed nutation residuals. J. Geod. 95, 57.",
+    "url": "https://doi.org/10.1007/s00190-021-01513-9"
   },
   {
-    'reference': "Rekier, J., Chao, B.F., Chen, J. et al. Earth's Rotation: Observations and Relation to Deep Interior. Surv Geophys (2021).",
-    'url': 'https://doi.org/10.1007/s10712-021-09669-x'
+    "reference": "Rekier, J., Chao, B.F., Chen, J., et al. (2021) Earth's Rotation: Observations and Relation to Deep Interior. Surv. Geophys.",
+    "url": "https://doi.org/10.1007/s10712-021-09669-x"
   },
   {
-    'reference': "Triana, S.A., Dumberry, M., Cébron, D. et al. Core Eigenmodes and their Impact on the Earth’s Rotation. Surv Geophys (2021).",
-    'url': 'https://doi.org/10.1007/s10712-021-09668-y'
-  }
+    "reference": "Triana, S.A., Dumberry, M., Cébron, D., Rekier, J., et al. (2021) Core Eigenmodes and their Impact on the Earth’s Rotation. Surv. Geophys.",
+    "url": "https://doi.org/10.1007/s10712-021-09668-y"
+  },
+  {
+    "reference": "J. Rekier (2022) Free Core Nutation and Its Relation to the Spin-over Mode. Planet. Sci. J. 3, 133.",
+    "url": "https://doi.org/10.3847/PSJ/ac6ce2"
+  },  
+  {
+    "reference": "S. A. Triana, G. Guerrero, A. Barik, J. Rekier (2022) Identification of Inertial Modes in the Solar Convection Zone. ApJL 934, L4.",
+    "url": "https://dx.doi.org/10.3847/2041-8213/ac7dac"
+  },
+  {
+    "reference": "M. Puica, V. Dehant, M. Folgueira, T. Van Hoolst, J. Rekier (2023) Analytical computation of total topographic torque at the core–mantle boundary and its impact on tidally driven length-of-day variations. Geophysical Journal International 234, 585–596.",
+    "url": "https://doi.org/10.1093/gji/ggad077"
+  },
+  {
+    "reference": "J. Rekier, S. A. Triana, A. Trinh, B. Buffett (2023) Electric interface condition for sliding and viscous contacts. Phys. Rev. Research 5, 033029.",
+    "url": "https://link.aps.org/doi/10.1103/PhysRevResearch.5.033029",
+    "arxiv": "[arxiv:2303.00718]",
+    "arxivurl": "http://arxiv.org/abs/2303.00718"
+  },
+  {
+    "reference": "F. Seuren, S. A. Triana, J. Rekier, A. Barik, T. Van Hoolst (2023) Effects of the Librationally Induced Flow in Mercury’s Fluid Core with an Outer Stably Stratified Layer. Planet. Sci. J. 4, 161.",
+    "url": "https://doi.org/10.3847/PSJ/acee77"
+  },  
+  {
+    "reference": "S.-A. (施勝安) Shih, S. A. Triana, J. Rekier, V. Dehant (2023) Turbulent dissipation in the boundary layer of precession-driven flow in a sphere. AIP Advances 13, 075025.",
+    "url": "https://doi.org/10.1063/5.0146932"
+  }    
 ]
